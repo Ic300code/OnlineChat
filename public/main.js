750008c6c274880ed5d9eab3c2c5fca3486a90fb
Ic300code <ifontenoygonier@gmail.com>
2025-02-05 15:29:00 +0100

    Add files via upload

---
+++ public/main.js
@@ -0,0 +1,42 @@
+const socket = io();
+
+function addChat() {
+    const chatInput = document.getElementById("chatinput").value;
+
+    const input = document.getElementById("name");
+    const name = input.value;
+    
+    socket.emit("chat", [chatInput, name]);
+
+    document.getElementById("chatinput").value = "";
+}
+
+function login() {
+    const loginContainer = document.querySelector(".login");
+    const chatContainer = document.querySelector(".chatContainer");
+    const newChatContainer = document.querySelector(".newChat");
+
+    const input = document.getElementById("name");
+    const name = input.value;
+
+    chatContainer.style.display = "flex";
+    newChatContainer.style.display = "flex";
+    loginContainer.style.display = "none";
+    
+}
+
+socket.on("chat", (chats) => {
+    const chatContainer = document.querySelector(".chatContainer");
+    chatContainer.innerHTML = "";
+
+    const input = document.getElementById("name");
+    const name = input.textContent;
+
+    chats.forEach((chats) => {
+        console.log(chats)
+
+        const chatElement = document.createElement("p");
+        chatElement.textContent = chats[1]+":"+chats[0];
+        chatContainer.appendChild(chatElement);
+    });
+});
